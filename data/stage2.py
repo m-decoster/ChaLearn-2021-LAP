fe27085e2497edd783828420a36270418a812ec4
Mathieu De Coster <m-decoster@users.noreply.github.com>
2021-03-12 14:32:46 +0100

    Update output file path of stage2.py

--- data/stage2.py
+++ data/stage2.py
@@ -2,7 +2,7 @@ import csv
 
 with open('train_labels.csv') as tlf:
     with open('val_labels.csv') as vlf:
-        with open('train_val_labels.csv', 'w') as olf:
+        with open('train_val_labels_STAGE2.csv', 'w') as olf:
             train_reader = csv.reader(tlf)
             val_reader = csv.reader(vlf)
             out_writer = csv.writer(olf)
